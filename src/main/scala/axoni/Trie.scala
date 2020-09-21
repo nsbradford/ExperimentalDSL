7bee7ec68a6c027ef2934da942e4f02abd4dec00
nsbradford <nsbradford@gmail.com>
2020-09-21 08:23:23 -0400

    Trie get() working.

--- src/main/scala/axoni/Trie.scala
+++ src/main/scala/axoni/Trie.scala
@@ -1,8 +1,77 @@
 package axoni
 
+
 /**
   * Created by nicholasbradford on 9/21/20.
   */
-class Trie {
+object Trie {
 
+  type uint = Int // Java/Scala don't have unsigned
+
+  def toBinaryLeastSigBitFirst(x: Int): Seq[Int] = {
+    x.toBinaryString
+      .reverse
+      .split("")
+      .map(_.toInt)
+  }
+
+}
+
+import Trie._
+
+import scala.annotation.tailrec
+import scala.collection.mutable
+
+
+/**
+  * Making this class mutable to meet problem specs easily.
+  *
+  * Purposefully making non-generic for simplicity;
+  *   Could alternatively have default behavior or typeclasses for converting various types to array-like types.
+  *
+  */
+class TrieNode(val children: mutable.Map[Int, TrieNode] = mutable.Map(),
+               var stored: Option[String] = None)
+{
+  // more generic than hardcoding {0,1} as children options (for future expansion of this class), at price of compile-time safety
+  require(children.keySet.forall(key => Set(0,1) contains key))
+
+  /** Decodes integer into binary representation (least sig bit first),
+    * and then stores into the Trie. */
+  def insert(key: uint, value: String): Unit = insertHelper(toBinaryLeastSigBitFirst(key), value)
+
+  @tailrec
+  private def insertHelper(key: Seq[Int], value: String): Unit = key match {
+    case Seq() =>
+      stored = Some(value)
+      ()
+    case head +: tail =>
+      val nextNode: TrieNode = children.getOrElse(head, {
+        val next = new TrieNode()
+        this.children += ((head, next))
+        next
+      })
+      nextNode.insertHelper(tail, value)
+  }
+
+
+  /** Returns the value for given key, or the empty string if the key is not found. */
+  def get(key: uint): String = getHelper(toBinaryLeastSigBitFirst(key))
+
+  @tailrec
+  private def getHelper(key: Seq[Int]): String = key match {
+    case Seq() =>
+      println(s"Reached end of search, returning stored value $stored")
+      stored.getOrElse("")
+    case head +: tail =>
+      println(s"Found $head +: $tail, searching children $children")
+      children.get(head) match {
+        case Some(trieNode) =>
+          println(s"\tFound child with $head, continuing search with $tail...")
+          trieNode.getHelper(tail)
+        case None =>
+          println(s"\tFound no children with $head, abandoning search.")
+          ""
+      }
+  }
 }
--- src/test/scala/axoni/TrieTest.scala
+++ src/test/scala/axoni/TrieTest.scala
@@ -1,8 +1,47 @@
 package axoni
 
+import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.wordspec.AnyWordSpec
+import Trie._
+
+import scala.collection.mutable
+
+
 /**
   * Created by nicholasbradford on 9/21/20.
   */
-class TrieTest {
+class TrieTest extends WordSpec with Matchers {
 
+
+  "Trie" when {
+    "toBinaryLeastSigBitFirst" should {
+      "convert properly" in {
+        4.toBinaryString shouldBe "100"
+        2.toBinaryString shouldBe "10"
+        toBinaryLeastSigBitFirst(4) shouldBe Array(0, 0, 1)
+        toBinaryLeastSigBitFirst(2) shouldBe Array(0, 1)
+      }
+    }
+
+    "get" should {
+      "return the empty string when prefix not in tree" in {
+        val empty = new TrieNode()
+        empty.get(4) shouldBe ""
+      }
+      "return the empty string when prefix is not a terminated node" in {
+        // assemble manually so doesn't have to test insert function
+        val expected = "answer"
+        val has4 = new TrieNode(children = mutable.Map(
+          0 -> new TrieNode(children = mutable.Map(
+            0 -> new TrieNode(children = mutable.Map(
+              1 -> new TrieNode(stored = Some(expected))
+            ))
+          ))
+        ))
+
+        val result = has4.get(4)
+        result shouldBe expected
+      }
+    }
+  }
 }
